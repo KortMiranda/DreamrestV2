b7e0ac65cd4aef46b8015698a316388c4fe785c0
kortmiranda <kortneymiranda@outlook.com>
2022-05-20 18:10:57 -0500

    connected to db

--- server/index.js
+++ server/index.js
@@ -9,4 +9,13 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://KortMiranda:dmXHLKcX8y35eAJn@cluster0.ekhlr.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false)
+
 // https://www.mongodb.com/cloud/atlas 
