8ceb9bab1be6b4fde1e1ba59c9306767e7c43313
kortmiranda <kortneymiranda@outlook.com>
2022-05-30 15:18:44 -0500

    add create post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import useStyles from './styles'
+import { useDispatch } from 'react-redux';
+import { createPosts } from  '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPosts(postData))
     }
 
     const clear = () => {
-        
+
     }
 
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">Creating a Memory</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
@@ -30,5 +35,6 @@ const Form = () => {
         </Paper>
     )
 }
+
 
 export default Form;
